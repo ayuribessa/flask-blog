5d725ec973c1b3248c6689ac5a5a95b1fc011bbe
Ayuri Bessa <ayuribessa@gmail.com>
2022-09-28 08:43:24 -0300

    adiciona rota e controler de update e delete

--- views.py
+++ views.py
@@ -95,3 +95,27 @@ def new_task():
         g.db.close()
         flash("New entry successfully posted.")
         return redirect(url_for('tasks'))
+
+#Mark task as complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.db.execute(
+        'update tasks set status = 0 where task_id='+str(task_id)
+    )
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as complete')
+    return redirect(url_for('tasks'))
+
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete(task_id):
+    g.db = connect_db()
+    g.db.execute('delete from tasks where task_id='+str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash('The task was deleted')
+    return redirect(url_for('tasks'))
+    
