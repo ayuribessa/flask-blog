845ee5eb9e6b64944507e688f2b1e33542a268c0
Ayuri Bessa <ayuribessa@gmail.com>
2022-10-29 11:35:00 -0300

    finalizando testes de usuário

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ class Alltests(unittest.TestCase):
             name="finish classes",
             due_date="02/11/2022",
             priority='1',
-            posted_date="25/10/2022",
+            posted_date="10/10/2022",
             status="1"
         ),follow_redirects=True)
     
@@ -113,16 +113,16 @@ class Alltests(unittest.TestCase):
         
     def test_users_can_add_tasks(self):
         self.create_user('Ayuri','ayuri@realpython.org','python')
-        self.login('ayuri','python')
+        self.login('Ayuri','python')
         self.app.get('/tasks',follow_redirects=True)
         response = self.create_task()
-        self.assertIn(b'New entry was successfully posted. Thanks.', response.data)
+        self.assertIn(b'New entry was successfully posted.', response.data)
         
     def test_users_cannot_add_tasks_when_error(self):
         self.create_user('Michael', 'michael@realpython.com', 'python')
         self.login('Michael', 'python')
         self.app.get('tasks/', follow_redirects=True)
-        response = self.app.post('add/', data=dict(
+        response = self.app.post('/add', data=dict(
             name='Go to the bank',
             due_date='',
             priority='1',
@@ -130,9 +130,35 @@ class Alltests(unittest.TestCase):
             status='1'
         ), follow_redirects=True)
         self.assertIn(b'This field is required.', response.data)
-        
 
+    def test_users_can_complete_tasks(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        response = self.app.get('complete/1/', follow_redirects=True)
+        self.assertIn(b'The task was marked as complete', response.data)
         
+    def test_users_can_delete_tasks(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        response = self.app.get('delete/1/', follow_redirects=True)
+        self.assertIn(b'The task was deleted', response.data)
+
+    def test_users_cannot_complete_tasks_that_are_not_created_by_them(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
+        self.login('Fletcher', 'python101')
+        self.app.get('tasks/', follow_redirects=True)
+        self.app.get('/tasks',follow_redirects=True)
+        response = self.app.get("complete/1/",follow_redirects=True)
+        self.assertNotIn(b'The task was marked as complete', response.data)
         
 if __name__ == "__main__":
     unittest.main()
