57ed2303882e8a6a9cdc8d5fd119b0a339b9ca93
Ayuri Bessa <ayuribessa@gmail.com>
2022-11-04 09:58:31 -0300

    adiciona user role no model, e atualiza migrations e views conforme

--- views.py
+++ views.py
@@ -42,6 +42,7 @@ def login_required(test):
 def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
+    session.pop('role', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -56,12 +57,11 @@ def login():
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
                 session['user_id'] = user.id
+                session['role'] = user.role
                 flash('Welcome!')
                 return redirect(url_for('tasks'))
             else:
                 error = 'Invalid username or password.'
-        else:
-            error = 'Both fields are required.'
     return render_template('login.html', form=form, error=error)
 
 
@@ -100,20 +100,28 @@ def new_task():
 @login_required
 def complete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
-    db.session.commit()
-    flash('The task was marked as complete.')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user.id'] == task.first().user_id or session['role'] == 'admin':
+        task.update({"status": "0"})
+        db.session.commit()
+        flash('The task was marked as complete.')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only update tasks that belong to you.')
 
 
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).delete()
-    db.session.commit()
-    flash('The task was deleted')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user.id'] == task.first().user_id or session['role'] == 'admin':
+        task.delete()
+        db.session.commit()
+        flash('The task was deleted')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only delete tasks that belong to you.')
 
     
 @app.route('/register/', methods=['GET', 'POST'])
