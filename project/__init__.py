45081ab3463f355a57649848d0282405da68b7c9
Ayuri Bessa <ayuribessa@gmail.com>
2022-11-16 12:53:25 -0300

    alteração controller de errors para criar arquivo com informações

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -17,8 +17,20 @@ app.register_blueprint(tasks_blueprint)
 
 @app.errorhandler(404)
 def not_found(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log','a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write(f'\n404 error: at {current_timestamp}: {r}\n')
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log','a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write(f'\n 404 error at : {current_timestamp}:  {r}\n')
     return render_template('500.html'),500
